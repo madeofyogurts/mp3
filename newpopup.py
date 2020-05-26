a06e28129aec3eeb2c29ced31f5badc862af7c74
Zuza <56550989+madeofyogurts@users.noreply.github.com>
2020-05-26 17:32:04 +0200

    Update newpopup.py
    Poprawiony kod
    (Nie do końca aktualny i tak, bo zostawiamy PyGame_GUI na rzecz wxPython, ale przydać się może!)

--- newpopup.py
+++ newpopup.py
@@ -6,29 +6,28 @@ import pygame_gui
 
 pygame.init()
 
-pygame.display.set_caption('mp3 Player')
-window_surface = pygame.display.set_mode((800, 600))
+(width, height) = (500, 500)
+popup = pygame.display.set_mode((width, height)) #Wielkość okienka
+pygame.display.set_caption('mp3 Player') #Tytuł okienka
 
-background = pygame.Surface((800, 600))
-background.fill(pygame.Color('#000000'))
+background = pygame.Surface((500, 500))
+background.fill(pygame.Color('#000000')) #Kolor tła
 
-manager = pygame_gui.UIManager((800, 600)) 
+manager = pygame_gui.UIManager((500, 500)) 
+playButtonOne = pygame_gui.elements.UIButton(relative_rect=pygame.Rect((50, 50), (400, 50)), text='"She Wants" by Metronomy', manager=manager)
+playButtonTwo = pygame_gui.elements.UIButton(relative_rect=pygame.Rect((50, 125), (400, 50)), text='"Ain\'t No Sunshine" by Bill Withers', manager=manager)
+playButtonThree = pygame_gui.elements.UIButton(relative_rect=pygame.Rect((50, 200), (400, 50)), text='"Lone Digger" by Caravan Palace', manager=manager)
+stopButton = pygame_gui.elements.UIButton(relative_rect=pygame.Rect((50, 400), (400, 50)), text='Pause the music', manager=manager)
+#Tutaj o są przyciski do odtwarzania muzyki
 
-playButtonOne = pygame_gui.elements.UIButton(relative_rect=pygame.Rect((200, 50), (400, 50)), text='"She Wants" by Metronomy', manager=manager)
-playButtonTwo = pygame_gui.elements.UIButton(relative_rect=pygame.Rect((200, 125), (400, 50)), text='"Ain\'t No Sunshine" by Bill Withers', manager=manager)
-playButtonThree = pygame_gui.elements.UIButton(relative_rect=pygame.Rect((200, 200), (400, 50)), text='"Lone Digger" by Caravan Palace', manager=manager)
-stopButton = pygame_gui.elements.UIButton(relative_rect=pygame.Rect((200, 400), (400, 50)), text='Pause the music', manager=manager)
+pygame.display.flip() 
 
-
-clock = pygame.time.Clock()
-is_running = True
-
-while is_running:
-    time_delta = clock.tick(60)/1000.0
+program = True
+while program:
     for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            is_running = False
-        #1
+        if event.type == pygame.QUIT: #Wyłączenie okienka
+            program = False
+
         if event.type == pygame.USEREVENT:
             if event.user_type == pygame_gui.UI_BUTTON_PRESSED:
                 if event.ui_element == playButtonOne:
@@ -36,8 +35,7 @@ while is_running:
                     pygame.mixer.init()
                     pygame.mixer.music.load("shewants.mp3")
                     pygame.mixer.music.play()
-
-        #2
+        
         if event.type == pygame.USEREVENT:
             if event.user_type == pygame_gui.UI_BUTTON_PRESSED:
                 if event.ui_element == playButtonTwo:
@@ -46,7 +44,6 @@ while is_running:
                     pygame.mixer.music.load("nosunshine.mp3")
                     pygame.mixer.music.play()
 
-        #3
         if event.type == pygame.USEREVENT:
             if event.user_type == pygame_gui.UI_BUTTON_PRESSED:
                 if event.ui_element == playButtonThree:
@@ -61,14 +58,8 @@ while is_running:
                     print('The music has stopped.')
                     pygame.mixer.init()
                     pygame.mixer.music.stop()
-        
-                
-
 
         manager.process_events(event)
-    manager.update(time_delta)
-
-    window_surface.blit(background, (0, 0))
-    manager.draw_ui(window_surface)
-
+    popup.blit(background,(0, 0))
+    manager.draw_ui(popup) #To tutaj pokazuje przyciski
     pygame.display.update()
