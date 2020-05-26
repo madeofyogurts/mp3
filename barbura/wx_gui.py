61038d694357f367ac5e4b0b7364a1bfec3f6582
barburaadamska <32602120+barburaadamska@users.noreply.github.com>
2020-05-26 21:44:47 +0200

    GUI styling update
    so - now you should be able to see more or less what our UI will look like.
    
    added: 
    - styling (both of the whole frame and of text)
    - more labels (Static Text)
    - button on the right
    
    to do: 
    - progress bar
    - shuffle and repeat icons
    - check how to change the appearance of toggle buttons depending on the state (if at all possible)

--- barbura/wx_gui.py
+++ barbura/wx_gui.py
@@ -4,22 +4,30 @@
 import wx
 
 
-def button_creation(path, width, height, parent, position_x, position_y, toggle=False):
+def bitmap_button_creation(path, width, height, parent, position_x, position_y, toggle=False):
     bitmap = wx.Bitmap(path)
     image = bitmap.ConvertToImage()
     image = image.Scale(width, height, wx.IMAGE_QUALITY_HIGH)
     result = wx.Bitmap(image)
     if not toggle:
         button_created = wx.BitmapButton(parent, pos=(position_x, position_y), size=(height, width), bitmap=result)
+
     else:
         button_created = wx.BitmapToggleButton(parent, pos=(position_x, position_y), size=(height, width), label=result)
     return button_created
 
 
-def label_creation(parent, user_text, fontsize):
-    label_created = wx.StaticText(parent, style=wx.ALIGN_CENTER)
+def label_creation(parent, user_text, fontsize, position_x, position_y, color, weight=False):
+    label_created = wx.StaticText(parent, style=wx.ALIGN_CENTER, pos=(position_x, position_y))
     label_text = str(user_text)
-    font = wx.Font(int(fontsize), wx.SWISS, wx.NORMAL, wx.NORMAL)
+    if weight == "bold":
+        label_weight = wx.BOLD
+    elif weight == "light":
+        label_weight = wx.LIGHT
+    if not weight:
+        label_weight = wx.NORMAL
+    font = wx.Font(int(fontsize), wx.SWISS, wx.NORMAL, label_weight)
+    label_created.SetForegroundColour(color)
     label_created.SetFont(font)
     label_created.SetLabel(label_text)
     return label_created
@@ -27,15 +35,30 @@ def label_creation(parent, user_text, fontsize):
 class MyFrame(wx.Frame):
     def __init__(self):
 
-        super().__init__(parent=None, title="MP3 Player")
+        super().__init__(parent=None, title='MP3 Player', size=(800, 300))
         panel = wx.Panel(self)
+        self.SetBackgroundColour('#ffffff')
 
-        lbl1 = label_creation(panel, "\n\tNow playing from", 18)
-        lbl2 = label_creation(panel, "\n\n\n\tTwoja Stara", 14)
+        # LEFT SIDE WITH LABELS
 
-        test_button = button_creation("icon_backward.png", 50, 50, panel, 50, 100)
-        play_button = button_creation("icon_play.png", 70, 70, panel, 120, 90, toggle=True)
-        forward_button = button_creation("icon_forward.png", 50, 50, panel, 210, 100)
+        playing_from_label = label_creation(panel, 'NOW PLAYING FROM', 13, 99, 54, '#707070')
+        user_whereabouts_label = label_creation(panel, 'Twoja Stara in Desktop', 17, 99, 79, '#FF2D55')
+
+        song_title_label = label_creation(panel, 'Song Title', 22, 99, 123, '#000000', 'bold')
+        artist_label = label_creation(panel, 'Artist Name', 17, 99, 157, '#707070')
+
+        time_from_beginning_label = label_creation(panel, '06:15', 11, 99, 217, '#FF2D55')
+        song_length_label = label_creation(panel, '21:37', 11, 391, 217, '#FF2D55')
+        # --------------------------------------
+
+        # RIGHT SIDE WITH BUTTONS
+
+        change_folder_button = wx.Button(panel, label='CHANGE FOLDER', pos=(510, 54), size=(183, 36))
+
+        test_button = bitmap_button_creation('icon_backward.png', 40, 40, panel, 510, 130)
+        play_button = bitmap_button_creation('icon_play.png', 80, 80, panel, 560, 110, toggle=True)
+        forward_button = bitmap_button_creation('icon_forward.png', 40, 40, panel, 650, 130)
+        # --------------------------------------
 
         self.Show()
 
